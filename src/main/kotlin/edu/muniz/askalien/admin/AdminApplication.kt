13c653cb3f199929bccb9d849821a5ad7699bd3b
mmuniz75 <mmunizs1975@gmail.com>
2021-03-05 08:06:15 -0300

    enable block hound check

--- src/main/kotlin/edu/muniz/askalien/admin/AdminApplication.kt
+++ src/main/kotlin/edu/muniz/askalien/admin/AdminApplication.kt
@@ -2,10 +2,18 @@ package edu.muniz.askalien.admin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import reactor.blockhound.BlockHound
+import java.lang.Boolean
+import java.util.*
 
 @SpringBootApplication
 class AdminApplication
 
 fun main(args: Array<String>) {
+
+	if (Objects.nonNull(System.getenv("BLOCKHOUND")) && Boolean.valueOf(System.getenv("BLOCKHOUND"))) {
+		BlockHound.install()
+	}
+
 	runApplication<AdminApplication>(*args)
 }
