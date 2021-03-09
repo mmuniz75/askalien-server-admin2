0f6d6f4202872212c0b1784703ae0e623fa778a1
mmuniz75 <mmunizs1975@gmail.com>
2021-03-09 08:48:20 -0300

    align answer field

--- src/main/kotlin/edu/muniz/askalien/admin/domain/AnswerSummary.kt
+++ src/main/kotlin/edu/muniz/askalien/admin/domain/AnswerSummary.kt
@@ -1,5 +1,6 @@
 package edu.muniz.askalien.admin.domain
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
@@ -8,6 +9,8 @@ data class AnswerSummary (
 
     @Id
     var id: Int? = null,
+
+    @JsonProperty("question")
     var subject: String? = null
 
 )
